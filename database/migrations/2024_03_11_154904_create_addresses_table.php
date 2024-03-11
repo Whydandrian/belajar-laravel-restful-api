9f5094285855be57ebb3018bc8e321de2b73b542
Whydandrian <whydandrian@gmail.com>
2024-03-11 22:59:09 +0700

    Address: add properties table

--- database/migrations/2024_03_11_154904_create_addresses_table.php
+++ database/migrations/2024_03_11_154904_create_addresses_table.php
@@ -6,22 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('addresses', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create('addresses', function (Blueprint $table) {
+         $table->id();
+         $table->string("street", 200)->nullable();
+         $table->string("city", 100)->nullable();
+         $table->string("province", 100)->nullable();
+         $table->string("country", 100)->nullable(false);
+         $table->string("postal_code", 10)->nullable();
+         $table->unsignedBigInteger("contact_id")->nullable(false);
+         $table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('addresses');
-    }
+         $table->foreign("contact_id")->on("contacts")->references("id");
+      });
+   }
+
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists('addresses');
+   }
 };
