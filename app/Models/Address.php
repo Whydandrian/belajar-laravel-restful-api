dbd2d8fcf921edb23593f9542a861c3fa8bec99c
Whydandrian <whydandrian@gmail.com>
2024-03-11 23:03:30 +0700

    Add relation contact & address

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
@@ -12,4 +13,9 @@ class Address extends Model
    protected $keyType = "int";
    public $incrementing = true;
    public $timestamps = true;
+
+   public function contact(): BelongsTo
+   {
+      return $this->belongsTo(Contact::class, "contact_id", "id");
+   }
 }
