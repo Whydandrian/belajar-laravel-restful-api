7e2fd64fa2cb55ad06574becdf04a7f302f281a8
Whydandrian <whydandrian@gmail.com>
2024-03-11 23:11:41 +0700

    UserResource: add response property

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -7,13 +7,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
-    }
+   /**
+    * Transform the resource into an array.
+    *
+    * @return array<string, mixed>
+    */
+   public function toArray(Request $request): array
+   {
+      return [
+         "id" => $this->id,
+         "username" => $this->username,
+         "name" => $this->name
+      ];
+   }
 }
