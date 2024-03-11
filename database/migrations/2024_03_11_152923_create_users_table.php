b13efd42bbddd41fab7fc0bb73f0334e7cfe752a
Whydandrian <whydandrian@gmail.com>
2024-03-11 22:36:15 +0700

    User: add table properties & property model

--- database/migrations/2024_03_11_152923_create_users_table.php
+++ database/migrations/2024_03_11_152923_create_users_table.php
@@ -6,22 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create('users', function (Blueprint $table) {
+         $table->id();
+         $table->string("username", 100)->nullable(false)->unique("users_username_unique");
+         $table->string("password", 100)->nullable(false);
+         $table->string("name", 100)->nullable(false);
+         $table->string("token", 100)->nullable()->unique("users_token_unique");
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists('users');
+   }
 };
