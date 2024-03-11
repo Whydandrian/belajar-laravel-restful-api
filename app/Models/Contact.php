82b2c8bec6bbe3b80b49cd344b949b9b9590511b
Whydandrian <whydandrian@gmail.com>
2024-03-11 22:44:21 +0700

    Contact: add properties in model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-   use HasFactory;
+   protected $table = "contacts";
+   protected $keyType = "int";
+   protected $primaryKey = "id";
+   public $incrementing = true;
+   public $timestamps = true;
 }
