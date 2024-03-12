0a20bd332baa5daa4f9f0c7cb136bc83f5fcd714
Whydandrian <whydandrian@gmail.com>
2024-03-12 21:07:04 +0700

    add logic testLogin success

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -68,6 +70,20 @@ class UserTest extends TestCase
 
    public function testLoginSuccess()
    {
+      $this->seed([UserSeeder::class]);
+      $this->post('/api/users/login', [
+         "username" => "test",
+         "password" => "test"
+      ])
+         ->assertStatus(200)
+         ->assertJson([
+            "data" => [
+               "username" => "test",
+               "password" => "test"
+            ]
+         ]);
+      $user = User::where("username", "test")->first();
+      self::assertNotNull($user->token);
    }
    public function testLoginFailed()
    {
