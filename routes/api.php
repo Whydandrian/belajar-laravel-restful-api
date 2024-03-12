e487693f87dfd8989f696e2cb1f8648a509bd5d0
Whydandrian <whydandrian@gmail.com>
2024-03-12 21:38:09 +0700

    add route get login user with middleware

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\ApiAuthenticationMiddleware;
 use App\Http\Middleware\ApiAuthMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\Middleware;
@@ -28,5 +29,9 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware(ApiAuthMiddleware::class)->group(function () {
 //    Route::get("/users/current", [UserController::class, 'get']);
 // });
+
 Route::post('/users', [UserController::class, 'register']);
 Route::post('/users/login', [UserController::class, 'login']);
+Route::middleware(ApiAuthenticationMiddleware::class)->group(function (){
+   Route::get('/users/current', [UserController::class, 'get']);
+});
