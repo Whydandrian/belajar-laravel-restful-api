5f88a7bc55a2212d938550d47d477ed950fa211a
Whydandrian <whydandrian@gmail.com>
2024-03-12 19:47:32 +0700

    LoginReq: add rules & add failed validation function

--- app/Http/Requests/UserLoginRequest.php
+++ app/Http/Requests/UserLoginRequest.php
@@ -2,20 +2,30 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserLoginRequest extends FormRequest
 {
 
    public function authorize(): bool
    {
-      return false;
+      return true;
    }
 
    public function rules(): array
    {
       return [
-         //
+         "username" => ["required", "max:100"],
+         "password" => ["required", "max:100"]
       ];
+   }
+
+   protected function failedValidation(Validator $validator)
+   {
+      throw new HttpResponseException(response([
+         "errors" => $validator->getMessageBag()
+      ], 400));
    }
 }
