61d74e515a33d822e8eaacb27c0c9e09eabda02a
Whydandrian <whydandrian@gmail.com>
2024-03-12 19:57:53 +0700

    Add route login user

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,4 @@ use Illuminate\Support\Facades\Route;
 //    Route::get("/users/current", [UserController::class, 'get']);
 // });
 Route::post('/users', [UserController::class, 'register']);
+Route::post('/users/login', [UserController::class, 'login']);
