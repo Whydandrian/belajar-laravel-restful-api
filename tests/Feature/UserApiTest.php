f3e42f84a43b51c1da3ce3b1a613e6a6de790de1
Whydandrian <whydandrian@gmail.com>
2024-03-12 10:13:39 +0700

    Artisan: make test user

--- tests/Feature/UserApiTest.php
+++ tests/Feature/UserApiTest.php
@@ -10,145 +10,148 @@ use Tests\TestCase;
 
 class UserApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function testRegisterSuccess()
-    {
-     $this->post('/api/users',[
-        'username'=> 'wahyudi',
-        'password'=> 'rahasia',
-        'name'=> 'wahyu',
-     ])->assertStatus(201)
-     ->assertJson([
-        "data"=>[
-            'username'=> 'wahyudi',
-            'name'=> 'wahyu',
-        ]
-     ]);
-    }
-    public function testRegisterFailed()
-    {
-        $this->post('/api/users',[
-            'username'=> '',
-            'password'=> '',
-            'name'=> '',
-         ])->assertStatus(400)
+   /**
+    * A basic feature test example.
+    */
+   public function testRegisterSuccess()
+   {
+      $this->post('/api/users', [
+         'username' => 'wahyudi',
+         'password' => 'rahasia',
+         'name' => 'wahyu',
+      ])->assertStatus(201)
          ->assertJson([
-            "errors"=>[
-                'username'=> [
-                    "The username field is required."
-                ],
-                'password'=> [
-                    "The password field is required."
-                ],
-                'name'=> [
-                    "The name field is required."
-                ],
-                
+            "data" => [
+               'username' => 'wahyudi',
+               'name' => 'wahyu',
             ]
          ]);
-    }
-    public function testRegisterUsernameAlreaedyExist()
-    {
-        $this->testRegisterSuccess();
+   }
+   public function testRegisterFailed()
+   {
+      $this->post('/api/users', [
+         'username' => '',
+         'password' => '',
+         'name' => '',
+      ])->assertStatus(400)
+         ->assertJson([
+            "errors" => [
+               'username' => [
+                  "The username field is required."
+               ],
+               'password' => [
+                  "The password field is required."
+               ],
+               'name' => [
+                  "The name field is required."
+               ],
 
-        $this->post('/api/users',[
-            'username'=> 'wahyudi',
-            'password'=> 'rahasia',
-            'name'=> 'wahyu',
-         ])->assertStatus(400)
-           ->assertJson([
-                "errors"=>[
-                    'username'=> [
-                        'username already registered'
-                    ],
-                ]
-            ]);
-    }
-    
-    public function testLoginSuccess()
-    {
-        $this->seed(UserSeeder::class);
-        $this->post('/api/users/login',[
-            'username'=> 'test',
-            'password'=> 'test',
-        ])->assertStatus(200)->assertJson([
-            'data'=>[
-                'username'=> 'test',
-                'name'=> 'test',
             ]
-        ]);
+         ]);
+   }
+   public function testRegisterUsernameAlreadyExist()
+   {
+      $this->testRegisterSuccess();
 
-        $user = User::where('username', 'test')->first();
-
-        self::assertNotNull($user->token);
-    }
-    public function testLoginFailedUsernameNotFound()
-    {
-        $this->post('/api/users/login',[
-            'username'=> 'test',
-            'password'=> 'test',
-        ])->assertStatus(401)->assertJson([
-            'errors'=>[
-                'message'=> [
-                    'username or password wrong'
-                ],
-                
+      $this->post('/api/users', [
+         'username' => 'wahyudi',
+         'password' => 'rahasia',
+         'name' => 'wahyu',
+      ])->assertStatus(400)
+         ->assertJson([
+            "errors" => [
+               'username' => [
+                  'username already registered'
+               ],
             ]
-        ]);
-    }
-    
-    public function testLoginPasswordWrong()
-    {
-        $this->seed(UserSeeder::class);
-        $this->post('/api/users/login',[
-            'username'=> 'test',
-            'password'=> 'salah',
-        ])->assertStatus(401)->assertJson([
-            'errors'=>[
-                'message'=> [
-                    'username or password wrong'
-                ],
-                
-            ]
-        ]);
-    }
+         ]);
+   }
 
-    public function testSuccess(){
-       $this->seed(UserSeeder::class); 
+   public function testLoginSuccess()
+   {
+      $this->seed(UserSeeder::class);
+      $this->post('/api/users/login', [
+         'username' => 'test',
+         'password' => 'test',
+      ])->assertStatus(200)->assertJson([
+         'data' => [
+            'username' => 'test',
+            'name' => 'test',
+         ]
+      ]);
 
-       $this->get('/api/users/current',[
-        'Authorization'=> 'test'
-        ])->assertStatus(200)->assertJson([
-            'data'=>[
-                'username'=> 'test',
-                'name'=> 'test',
+      $user = User::where('username', 'test')->first();
+
+      self::assertNotNull($user->token);
+   }
+   public function testLoginFailedUsernameNotFound()
+   {
+      $this->post('/api/users/login', [
+         'username' => 'test',
+         'password' => 'test',
+      ])->assertStatus(401)->assertJson([
+         'errors' => [
+            'message' => [
+               'username or password wrong'
             ],
-        ]);
-    }
-    public function testGetUnauthorized(){
-        $this->seed(UserSeeder::class); 
 
-       $this->get('/api/users/current')->assertStatus(401)->assertJson([
-            'errors'=>[
-                'message'=> [
-                    'unauthorized'
-                ],
-            ]
-        ]);
-    }
-    public function testGetInvalidToken(){
-        $this->seed(UserSeeder::class); 
+         ]
+      ]);
+   }
 
-       $this->get('/api/users/current',[
-        'Authorization'=> 'salah'
-        ])->assertStatus(401)->assertJson([
-            'errors'=>[
-                'message'=> [
-                    'unauthorized'
-                ],
-            ]
-        ]);
-    }
+   public function testLoginPasswordWrong()
+   {
+      $this->seed(UserSeeder::class);
+      $this->post('/api/users/login', [
+         'username' => 'test',
+         'password' => 'salah',
+      ])->assertStatus(401)->assertJson([
+         'errors' => [
+            'message' => [
+               'username or password wrong'
+            ],
+
+         ]
+      ]);
+   }
+
+   public function testSuccess()
+   {
+      $this->seed(UserSeeder::class);
+
+      $this->get('/api/users/current', [
+         'Authorization' => 'test'
+      ])->assertStatus(200)->assertJson([
+         'data' => [
+            'username' => 'test',
+            'name' => 'test',
+         ],
+      ]);
+   }
+   public function testGetUnauthorized()
+   {
+      $this->seed(UserSeeder::class);
+
+      $this->get('/api/users/current')->assertStatus(401)->assertJson([
+         'errors' => [
+            'message' => [
+               'unauthorized'
+            ],
+         ]
+      ]);
+   }
+   public function testGetInvalidToken()
+   {
+      $this->seed(UserSeeder::class);
+
+      $this->get('/api/users/current', [
+         'Authorization' => 'salah'
+      ])->assertStatus(401)->assertJson([
+         'errors' => [
+            'message' => [
+               'unauthorized'
+            ],
+         ]
+      ]);
+   }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,13 +7,13 @@ use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
-    protected function setUp(): void {
-        parent::setUp();
-        DB::delete('delete from addresses');
-        DB::delete('delete from contacts');
-        DB::delete('delete from users');
-    }
+   use CreatesApplication;
+   protected function setUp(): void {
+      parent::setUp();
+      DB::delete('delete from addresses');
+      DB::delete('delete from contacts');
+      DB::delete('delete from users');
+   }
 
 
 }
