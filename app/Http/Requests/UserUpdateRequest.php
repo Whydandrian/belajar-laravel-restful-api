4a16cdf1b6f1a14e3519df649dbd3ed90d9fdee0
Whydandrian <whydandrian@gmail.com>
2024-03-13 10:20:45 +0700

    Add rules & validation for update user request

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -2,27 +2,34 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
+   /**
+    * Determine if the user is authorized to make this request.
+    */
+   public function authorize(): bool
+   {
+      // User bisa melakukan update jika user yang login tidak sama dengan null
+      // atau jika sistem mendeteksi adanya user
+      return $this->user() != null;
+   }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+   public function rules(): array
+   {
+      return [
+         "name" => ["nullable", "max:100"],
+         "password" => ["nullable", "max:100"],
+      ];
+   }
+
+   protected function failedValidation(Validator $validator)
+   {
+      throw new HttpResponseException(response([
+         "errors" => $validator->getMessageBag()
+      ], 400));
+   }
 }
